7ac38b4f68a6f58b5527a727974c0e77dada44b8
Josh Hawes <joshua.m.hawes@gmail.com>
2024-04-15 16:56:47 +0000

    added home app and templates

--- oenotrus/settings.py
+++ oenotrus/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'django.contrib.sites',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -87,7 +88,10 @@ ROOT_URLCONF = 'oenotrus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
