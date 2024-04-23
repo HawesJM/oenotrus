c8eb9276dd98f4ffc4ebed14728cc85cb888599d
Josh Hawes <joshua.m.hawes@gmail.com>
2024-04-23 20:35:37 +0000

    added category filter function

--- products/views.py
+++ products/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 def all_wines(request):
     """A view to show all wines and allow sorting and searching """
 
     wines = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            wines = wines.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You did not enter any search criteria")
-                return redirect(reverse('products'))
+                return redirect(reverse('wines'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             wines = wines.filter(queries)
@@ -22,6 +28,7 @@ def all_wines(request):
     context = {
         'wines': wines,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/wines.html', context)
