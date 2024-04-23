f6faa1a9e6f2a4fefa5ad8bed790805dd064feec
Josh Hawes <joshua.m.hawes@gmail.com>
2024-04-23 19:00:20 +0000

    added search function

--- products/views.py
+++ products/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 def all_wines(request):
     """A view to show all wines and allow sorting and searching """
 
     wines = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did not enter any search criteria")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            wines = wines.filter(queries)
 
     context = {
         'wines': wines,
+        'search_term': query,
     }
 
     return render(request, 'products/wines.html', context)
