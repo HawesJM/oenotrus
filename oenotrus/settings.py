861e9380c7b58ce39355c6d4eb5c8297c5c8a51e
Josh Hawes <joshua.m.hawes@gmail.com>
2024-04-24 20:58:06 +0000

    added cart contexts

--- oenotrus/settings.py
+++ oenotrus/settings.py
@@ -101,6 +101,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -174,3 +175,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 20
