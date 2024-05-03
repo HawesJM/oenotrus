8a1afde8ee2dee4ad94ad0b93e42813db238c4cb
Josh Hawes <joshua.m.hawes@gmail.com>
2024-05-03 11:15:56 +0000

    added cache control

--- oenotrus/settings.py
+++ oenotrus/settings.py
@@ -211,6 +211,12 @@ FREE_DELIVERY_THRESHOLD = 100
 STANDARD_DELIVERY_PERCENTAGE = 20
 
 if 'USE_AWS' in os.environ:
+
+     AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'hawesjm-oenotrus'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
