7fea5d41efeee46cf298894bd63c31827e6778c3
Josh Hawes <joshua.m.hawes@gmail.com>
2024-05-03 06:49:19 +0000

    removed secret key and set debug

--- oenotrus/settings.py
+++ oenotrus/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m-o-*!cxx%vg37r&oqc&zyvi$lz+mm8!-dngf%dc^enjrjh$z0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['oenotrus-b46392d15346.herokuapp.com', '8000-hawesjm-oenotrus-t4gvkijgira.ws-eu110.gitpod.io']
