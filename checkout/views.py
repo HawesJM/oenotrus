762c570bb6600aec29625b5cf3a61c7cb206e3bd
Josh Hawes <joshua.m.hawes@gmail.com>
2024-05-14 17:58:24 +0000

    changed footer icon color for accessibility

--- checkout/views.py
+++ checkout/views.py
@@ -144,7 +144,6 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    
 
 
     # Save the user's info
